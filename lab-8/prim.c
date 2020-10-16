92975ba6bbf8780e409c4941b64f7e6a83ceeab4
shrujaya <shrujaya23@gmail.com>
2020-10-16 16:37:00 +0530

    Fixed prim.c

--- lab-8/prim.c
+++ lab-8/prim.c
@@ -1,66 +1,83 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <limits.h>
-#define SIZE 30
+#include<stdio.h>
+#include<stdlib.h>
+#define INF 9999999
+#define MAX 20
 
-int min(int adjlist[], bool visited[], int n) {
-    int temp = INT_MAX, pos = -1;
-    for (int i = 0; i < n; i++) {
-        if (!visited[i]) {
-            if (adjlist[i] < temp) {
-                temp = adjlist[i];
-                pos = i;
-            }
-        }
-    }
-    return pos;
-}
+int graph[MAX][MAX], spanningtree[MAX][MAX], n;
 
-int spanningtree(int graph[SIZE][SIZE], int n) {
-    int parent[SIZE], key[SIZE];
-    bool visited[SIZE];
+int prims() {
+	int cost[MAX][MAX];
+	int u, v, mindist, dist[MAX], from[MAX];
+	int visited[MAX], edges, mincost;
 
-    for (int i = 0; i < n; i++) {
-        key[i] = INT_MAX;
-        visited[i] = false;
-        parent[i] = -1;
-    }
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++) {
+			if(graph[i][j] == 0)
+				cost[i][j] = INF;
+			else
+				cost[i][j] = graph[i][j];
+				spanningtree[i][j] = 0;
+		}
 
-    key[0] = 0;
-    for (int i = 0; i < n - 1; i++) {
-        int u = min(key, visited, n);
-        visited[u] = true;
-        for (int j = 0; j < n; j++) {
-            if (graph[u][j] != 0 && !visited[j] && graph[u][j] < key[j]) {
-                parent[j] = u;
-                key[j] = graph[u][j];
-            }
-        }
-    }
+	dist[0] = 0;
+	visited[0] = 1;
 
-    int retval = 0;
-    for (int i = 1; i < n; i++) {
-        printf("%d --> %d : %d\n", parent[i], i, graph[i][parent[i]]);
-        retval += graph[i][parent[i]];
-    }
-    return retval;
+	for(int i = 1; i < n; i++) {
+		dist[i] = cost[0][i];
+		from[i] = 0;
+		visited[i] = 0;
+	}
+
+	mincost = 0;
+	edges = n-1;
+
+	while(edges > 0) {
+		mindist = INF;
+		for(int i = 1; i < n; i++)
+			if(visited[i] == 0 && dist[i] < mindist) {
+				v = i;
+				mindist = dist[i];
+			}
+
+		u = from[v];
+
+		spanningtree[u][v] = dist[v];
+		spanningtree[v][u] = dist[v];
+		edges--;
+		visited[v] = 1;
+
+		for(int i = 1; i < n; i++)
+			if(visited[i] == 0 && cost[i][v] < dist[i]) {
+				dist[i] = cost[i][v];
+				from[i] = v;
+			}
+
+		mincost = mincost + cost[u][v];
+	}
+
+	return(mincost);
 }
 
 void main() {
-    int graph[SIZE][SIZE];
-    int n;
-    printf("Enter the dimensions of your adjacency list: ");
-    scanf("%d", &n);
+	int total_cost;
+	printf("Enter the number of vertices: ");
+	scanf("%d", &n);
 
-    printf("Enter your adjacency list:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &graph[i][j]);
-        }
+	printf("Enter the adjacency matrix:\n");
+
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			scanf("%d", &graph[i][j]);
+
+	total_cost = prims();
+	printf("\n");
+	printf("Spanning tree matrix:\n");
+
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			printf("%d ", spanningtree[i][j]);
+    	}
+		printf("\n");
     }
-
-    printf("\n");
-    int mincost = spanningtree(graph, n);
-    printf("Minimum Cost = %d\n", mincost);
+	printf("Minimum Cost = %d\n", total_cost);
 }
